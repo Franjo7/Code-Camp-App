0ea44470d4145f6b86c6c20780a9d1f681a29e36
Franjo Lovrić <franjolovric23@gmail.com>
2024-03-20 09:11:59 +0100

    Dodana validacija za prijavu

--- frontend/src/app/(auth)/login/page.js
+++ frontend/src/app/(auth)/login/page.js
@@ -1,39 +1,53 @@
 "use client"
-import React from 'react'
-import { yupResolver } from '@hookform/resolvers/yup'
-import { useForm } from 'react-hook-form'
-import * as yup from 'yup'
+import React, { useEffect } from 'react';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
 
 const schema = yup.object().shape({
-  email: yup.string().required('Email is required').email('Email must be a valid email address'),
-  password: yup.string().required('Password is required')
-})
+  email: yup
+    .string()
+    .required('Email is required')
+    .email('Email must be a valid email address'),
+  password: yup
+    .string()
+    .required('Password is required')
+    .min(8, 'Password must be at least 8 characters long'),
+});
 
 const page = () => {
+  const { register, handleSubmit, reset, formState: { errors, isDirty, dirtyFields, isValid }, trigger } = useForm({
+    resolver: yupResolver(schema),
+    mode: 'onChange',
+  });
 
-  const {register, handleSubmit, formState: {errors}} = useForm({
-    resolver: yupResolver(schema)
-  })
+  useEffect(() => {
+    trigger();
+  }, [trigger]);
 
-  const formSubmit = (data) => {
-    console.log(data)
-  }
+  const formSubmit = (data, e) => {
+    e.preventDefault();
+    console.log(data);
+    reset();
+  };
 
   return (
-    <section className='mt-4'>
+    <section className='mt-10'>
       <h1 className='text-center text-white text-4xl'>Login</h1>
       <form className='flex flex-col gap-2 max-w-md mx-auto mt-5' autoComplete='off' onSubmit={handleSubmit(formSubmit)}>
-        <input type='email' name='email' placeholder='Your Email' className='input' {...register('email')} />
-        <p className='error-message'>{errors.email?.message}</p>
-        <input type='password' name='password' placeholder='Your Password' className='input' {...register('password')} />
-        <p className='error-message'>{errors.password?.message}</p>
-        <button type='submit' className='bg-blue-500 text-white rounded-md p-3'>Login</button>
+        <input type='email' placeholder='Your Email' className='input' {...register('email')} />
+        <p className='error-message'>{dirtyFields.email && errors.email?.message}</p>
+        <input type='password' placeholder='Your Password' className='input' {...register('password')} />
+        <p className='error-message'>{dirtyFields.password && errors.password?.message}</p>
+        <a href='/forgot-password' className='text-white text-right'>Forgot Password?</a>
+        <button type='submit' className={`button rounded-md p-3 ${!isValid ? 'disabled-button' : 'enabled-button'}`} 
+        disabled={!isValid}>Login</button>
       </form>
       <p className='text-center text-white mt-4'>
         Don't have an account? <a href='/register' className='underline'>Register</a>
       </p>
     </section>
-  )
-}
+  );
+};
 
-export default page
+export default page;
