9976006be9a9ea7f19c9b1a5eb7b6f5d84c98bab
Franjo Lovrić <franjolovric23@gmail.com>
2024-03-27 13:33:38 +0100

    Dodana mogućnost uređivanja polja za ažuriranje korisnika

--- frontend/src/app/admin/edit/[id]/page.js
+++ frontend/src/app/admin/edit/[id]/page.js
@@ -22,16 +22,43 @@ export default function EditUser() {
     }
   }, [id]);
 
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const userDataToUpdate = {
+        firstName: user.firstName,
+        lastName: user.lastName,
+        tel: user.tel,
+        email: user.email,
+        role: user.role
+      };
+  
+      await axios.put(process.env.NEXT_PUBLIC_URL_USER + `user/update/${id}`, userDataToUpdate);
+      alert('User updated successfully');
+    } catch (error) {
+      console.error('Error updating user:', error);
+    }
+  };  
+
   return (
     <section className='mt-10'>
       <h1 className='text-center text-white text-4xl font-bold'>Update User</h1>
-      <form className='flex flex-col gap-2 max-w-md mx-auto mt-5'>
-      <input type='text' placeholder={user.firstName} className='input' />
-      <input type='text' placeholder={user.lastName} className='input' />
-      <input type='text' placeholder={user.tel} className='input' />
-      <input type='email' placeholder={user.email} className='input' />
-      <input type='text' placeholder={user.role} className='input' />
-      <button type='submit' className='button rounded-md p-3 enabled-button'>Update</button>
+      <form className='flex flex-col gap-2 max-w-md mx-auto mt-5' onSubmit={handleSubmit}>
+        <input type='text' name='firstName' placeholder='First Name' className='input' value={user.firstName || ''} onChange={handleInputChange} />
+        <input type='text' name='lastName' placeholder='Last Name' className='input' value={user.lastName || ''} onChange={handleInputChange} />
+        <input type='text' name='tel' placeholder='Tel' className='input' value={user.tel || ''} onChange={handleInputChange} />
+        <input type='email' name='email' placeholder='Email' className='input' value={user.email || ''} onChange={handleInputChange} />
+        <select name='role' className='input' value={user.role || ''  } onChange={handleInputChange}>
+          <option value=''>Select Role</option>
+          <option value='admin'>Admin</option>
+          <option value='user'>User</option>
+        </select>
+        <button type='submit' className='button rounded-md p-3 enabled-button'>Update</button>
       </form>
     </section>
   );
