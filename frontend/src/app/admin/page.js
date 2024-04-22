327ae7ace131923429cd5698a9e7a7742f24d0d7
Franjo Lovrić <franjolovric23@gmail.com>
2024-04-22 14:36:08 +0200

    Dodani search i pagination kod prikaza svih korisnika i prijava, poboljšan izgled navedenih sučelja

--- frontend/src/app/admin/page.js
+++ frontend/src/app/admin/page.js
@@ -2,33 +2,31 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { FaEdit, FaTrash } from 'react-icons/fa';
-import { BrowserRouter as Router } from 'react-router-dom';
 import { useRouter } from 'next/navigation';
 import toast from 'react-hot-toast';
 
 export default function AdminPage() {
   const [data, setData] = useState([]);
-  const [showAlert, setShowAlert] = useState(false);
-  const [alertMessage, setAlertMessage] = useState('');
-  const router = useRouter();
   const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [usersPerPage] = useState(6);
+  const router = useRouter();
 
   useEffect(() => {
     async function getUsers() {
       try {
         setLoading(true);
         const token = localStorage.getItem('user._id');
-        const headers = {Authorization: `Bearer ${token}`};
-        const response = await axios.get(process.env.NEXT_PUBLIC_URL_USER + 'admin/users', {headers});
+        const headers = { Authorization: `Bearer ${token}` };
+        const response = await axios.get(process.env.NEXT_PUBLIC_URL_USER + 'admin/users', { headers });
         setData(response.data);
         setLoading(false);
-      } 
-      catch (error) {
+      } catch (error) {
         const token = localStorage.getItem('user._id');
         if (!token) {
           router.push('/login');
-        }
-        else {
+        } else {
           router.push('/');
         }
       }
@@ -36,19 +34,35 @@ export default function AdminPage() {
     getUsers();
   }, []);
 
+  const indexOfLastUser = currentPage * usersPerPage;
+  const indexOfFirstUser = indexOfLastUser - usersPerPage;
+  const currentUsers = data.slice(indexOfFirstUser, indexOfLastUser);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredUsers = currentUsers.filter(user =>
+    user.firstName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    user.lastName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    user.email.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   async function deleteUser(id) {
     try {
       const token = localStorage.getItem('user._id');
-      const headers = {Authorization: `Bearer ${token}`};
-      await axios.delete(process.env.NEXT_PUBLIC_URL_USER + `admin/delete/${id}`, {headers}); 
+      const headers = { Authorization: `Bearer ${token}` };
+      await axios.delete(process.env.NEXT_PUBLIC_URL_USER + `admin/delete/${id}`, { headers });
       setData(prevData => prevData.filter(user => user._id !== id));
-      toast.success('User deleted successfully');
+      toast.success('User deleted successfully!');
     } catch (error) {
       if (error.response && error.response.data && error.response.data.error) {
         const errorMessage = error.response.data.error.message;
         const errorCode = error.response.data.error.code;
         toast.error(`Error code: ${errorCode}, Message: ${errorMessage}`);
-    } else {
+      } else {
         console.error('An error occurred:', error);
       }
     }
@@ -60,47 +74,65 @@ export default function AdminPage() {
 
   return (
     <div className="container">
-      {showAlert && (
-        <div className="alert" onClick={() => setShowAlert(false)}>
-          {alertMessage}
-        </div>
-      )}
       {!loading && (
-      <div className="overflow-x-auto">
-        <table className="w-full table-fixed text-center">
-          <caption className='main-title'>Users</caption>
-          <thead>
-            <tr>
-              <th className="w-1/6 py-2">First Name</th>
-              <th className="w-1/6 py-2">Last Name</th>
-              <th className="w-1/6 py-2">Phone Number</th>
-              <th className="w-1/6 py-2">Email</th>
-              <th className="w-1/6 py-2">Role</th>
-              <th className="w-1/6 py-2">Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((user, index) => (
-              <tr key={index}>
-                <td className="w-1/6 py-2 break-all">{user.firstName}</td>
-                <td className="w-1/6 py-2 break-all">{user.lastName}</td>
-                <td className="w-1/6 py-2 break-all">{user.tel}</td>
-                <td className="w-1/6 py-2 break-all">{user.email}</td>
-                <td className="w-1/6 py-2 break-all">{user.role}</td>
-                <td className="w-1/6 py-2">
-                  <Router>
-                    <button className="btn btn-update" onClick={() => redirectToUpdate(user._id)}><FaEdit /></button>
-                    <button className="btn btn-delete" onClick={() => {
-                      if (window.confirm(`Are you sure you want to delete user ${user.firstName} ${user.lastName}?`)) {
-                        deleteUser(user._id);
-                      }
-                    }}><FaTrash /></button>
-                  </Router>
-                </td>
-              </tr>
-            ))}
-            </tbody>
-          </table>
+        <div>
+          <h1 className="main-title text-center mb-8 text-4xl font-bold">Users</h1>
+          <div className="search-bar mb-4">
+            <input
+              type="text"
+              placeholder="Search by name or email..."
+              value={searchTerm}
+              onChange={handleSearch}
+              className="border rounded-md p-2 w-full"
+            />
+          </div>
+          <div className="overflow-x-auto">
+            <table className="w-full table-fixed text-center">
+              <thead>
+                <tr>
+                  <th className="w-1/6 py-2">First Name</th>
+                  <th className="w-1/6 py-2">Last Name</th>
+                  <th className="w-1/6 py-2">Phone Number</th>
+                  <th className="w-1/6 py-2">Email</th>
+                  <th className="w-1/6 py-2">Role</th>
+                  <th className="w-1/6 py-2">Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {filteredUsers.map((user, index) => (
+                  <tr key={index}>
+                    <td className="w-1/6 py-2 break-all">{user.firstName}</td>
+                    <td className="w-1/6 py-2 break-all">{user.lastName}</td>
+                    <td className="w-1/6 py-2 break-all">{user.tel}</td>
+                    <td className="w-1/6 py-2 break-all">{user.email}</td>
+                    <td className="w-1/6 py-2 break-all">{user.role}</td>
+                    <td className="w-1/6 py-2">
+                      <button className="btn btn-update" onClick={() => redirectToUpdate(user._id)}><FaEdit /></button>
+                      <button className="btn btn-delete" onClick={() => {
+                        if (window.confirm(`Are you sure you want to delete user ${user.firstName} ${user.lastName}?`)) {
+                          deleteUser(user._id);
+                        }
+                      }}><FaTrash /></button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          <div className="pagination">
+            <ul className="flex justify-center space-x-4 p-4 m-4">
+              {Array.from({ length: Math.ceil(data.length / usersPerPage) }, (_, index) => (
+                <li key={index}>
+                  <button
+                    onClick={() => paginate(index + 1)}
+                    style={{ padding: '8px 12px', borderRadius: '4px', cursor: 'pointer', backgroundColor: currentPage === index + 1 ? '#4a90e2' : '#e0e0e0', color: currentPage === index + 1 ? '#ffffff' : '#000000' }}
+                  >
+                    {index + 1}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       )}
     </div>
--- frontend/src/app/applications/page.js
+++ frontend/src/app/applications/page.js
@@ -8,6 +8,9 @@ import toast from 'react-hot-toast';
 export default function ApplicationsPage() {
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [currentPage, setCurrentPage] = useState(1);
+    const [applicationsPerPage] = useState(6);
     const router = useRouter();
 
     useEffect(() => {
@@ -26,6 +29,20 @@ export default function ApplicationsPage() {
         }
         getApplications();
     }, []);
+
+    const indexOfLastApplication = currentPage * applicationsPerPage;
+    const indexOfFirstApplication = indexOfLastApplication - applicationsPerPage;
+    const currentApplications = data.slice(indexOfFirstApplication, indexOfLastApplication);
+
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+    const handleSearch = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    const filteredApplications = currentApplications.filter(application => 
+        application.user.toLowerCase().includes(searchTerm.toLowerCase())
+    );
 
     async function deleteApplication(id) {
         try {
@@ -47,47 +64,56 @@ export default function ApplicationsPage() {
     return (
         <div className="container">
             {!loading && (
-                <div className="overflow-x-auto">
-                    <h1 className="main-title text-center mb-4">Applications</h1>
-                    <table className="w-full table-fixed text-center">
-                        <thead>
-                            <tr>
-                                <th className="w-1/7 py-2 break-all">User</th>
-                                <th className="w-1/7 py-2 break-all">Workshop</th>
-                                <th className="w-1/7 py-2 break-all">Registration Date</th>
-                                <th className="w-1/7 py-2 break-all">Status</th>
-                                <th className="w-1/7 py-2 break-all">Points</th>
-                                <th className="w-1/7 py-2 break-all">Evaluation</th>
-                                <th className="w-1/7 py-2 break-all">Remark</th>
-                                <th className="w-1/7 py-2 break-all">Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {data.map(application => (
-                                <tr key={application._id}>
-                                    <td className="w-1/7 py-2 break-all">{application.user}</td>
-                                    <td className="w-1/7 py-2 break-all">{application.workshop}</td>
-                                    <td className="w-1/7 py-2 break-all">{application.registrationDate}</td>
-                                    <td className="w-1/7 py-2 break-all">{application.status}</td>
-                                    <td className="w-1/7 py-2 break-all">{application.points}</td>
-                                    <td className="w-1/7 py-2 break-all">{application.evaluation}</td>
-                                    <td className="w-1/7 py-2 break-all">{application.remark}</td>
-                                    <td className="w-1/7 py-2">
-                                        <button className="btn btn-update" onClick={() => redirectToUpdate(application._id)}>
-                                            <FaEdit />
-                                        </button>
-                                        <button className="btn btn-delete" onClick={() => {
-                                            if (window.confirm(`Are you sure you want to delete ${application.user}'s application?`)) {
-                                                deleteApplication(application._id);
-                                            }
-                                        }}>
-                                            <FaTrash />
-                                        </button>
-                                    </td>
-                                </tr>
+                <div>
+                    <h1 className="main-title text-center mb-8 text-4xl font-bold">Applications</h1>
+                    <div className="search-bar mb-4">
+                        <input 
+                            type="text" 
+                            placeholder="Search by user..." 
+                            value={searchTerm} 
+                            onChange={handleSearch} 
+                            className="border rounded-md p-2 w-full"
+                        />
+                    </div>
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 text-white">
+                        {filteredApplications.map(application => (
+                            <div key={application._id} className="border rounded-lg shadow-md p-6">
+                                <h2 className="text-2xl font-semibold mb-4">{application.user}</h2>
+                                <p className="mb-2"><strong>Workshop:</strong> {application.workshop}</p>
+                                <p className="mb-2"><strong>Registration Date:</strong> {application.registrationDate}</p>
+                                <p className="mb-2"><strong>Status:</strong> {application.status}</p>
+                                <p className="mb-2"><strong>Points:</strong> {application.points}</p>
+                                <p className="mb-2"><strong>Evaluation:</strong> {application.evaluation}</p>
+                                <p className="mb-2"><strong>Remark:</strong> {application.remark}</p>
+                                <div className="mt-6 flex justify-end space-x-1">
+                                    <button className="btn btn-update" onClick={() => redirectToUpdate(application._id)}>
+                                        <FaEdit />
+                                    </button>
+                                    <button className="btn btn-delete" onClick={() => {
+                                        if (window.confirm(`Are you sure you want to delete ${application.user}'s application?`)) {
+                                            deleteApplication(application._id);
+                                        }
+                                    }}>
+                                        <FaTrash />
+                                    </button>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                    <div className="pagination">
+                        <ul className="flex justify-center space-x-4 p-4 m-4">
+                            {Array.from({ length: Math.ceil(data.length / applicationsPerPage) }, (_, index) => (
+                                <li key={index}>
+                                    <button 
+                                        onClick={() => paginate(index + 1)} 
+                                        style={{ padding: '8px 12px', borderRadius: '4px', cursor: 'pointer', backgroundColor: currentPage === index + 1 ? '#4a90e2' : '#e0e0e0', color: currentPage === index + 1 ? '#ffffff' : '#000000' }}
+                                    >
+                                        {index + 1}
+                                    </button>
+                                </li>
                             ))}
-                        </tbody>
-                    </table>
+                        </ul>
+                    </div>
                 </div>
             )}
         </div>
