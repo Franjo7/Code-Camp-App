1ab4143ce5600e89cf5892cb7632b90b957ab8b8
Franjo Lovrić <franjolovric23@gmail.com>
2024-08-21 11:09:59 +0200

    Unaprijeđena validacija kod kreiranja nove radionice

--- frontend/src/app/workshops/create/page.js
+++ frontend/src/app/workshops/create/page.js
@@ -1,26 +1,47 @@
-"use client"
+"use client";
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { toast } from 'react-hot-toast';
 import { useRouter } from 'next/navigation';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+const schema = yup.object().shape({
+  name: yup
+    .string()
+    .required('Name is required'),
+  description: yup
+    .string()
+    .required('Description is required')
+    .min(200, 'Description should have at least 200 characters')
+    .max(1000, 'Description should not exceed 1000 characters'),
+  startDate: yup
+    .date()
+    .required('Start Date is required')
+    .min(new Date(), 'Invalid Start Date'),
+  endDate: yup
+    .date()
+    .required('End Date is required')
+    .min(yup.ref('startDate'), 'Invalid End Date'),
+  professor: yup
+    .string()
+    .required('Professor not selected'),
+});
 
 export default function WorkshopCreate() {
-  const [workshop, setWorkshop] = useState({
-    name: '',
-    description: '',
-    startDate: '',
-    endDate: '',
-    professor: ''
+  const { register, handleSubmit, formState: { errors, isValid }} = useForm({
+    resolver: yupResolver(schema),
+    mode: 'onChange',
   });
+
   const [professors, setProfessors] = useState([]);
   const router = useRouter();
-  const [isDirty, setIsDirty] = useState(false);
 
   useEffect(() => {
     async function getProfessors() {
       try {
         const response = await axios.get(process.env.NEXT_PUBLIC_URL_USER + 'user/getProfessors');
-        console.log('Professors:', response.data);
         setProfessors(response.data);
       } catch (error) {
         console.error('Error fetching professors:', error);
@@ -29,52 +50,31 @@ export default function WorkshopCreate() {
     getProfessors();
   }, []);
 
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setWorkshop({ ...workshop, [name]: value });
-    setIsDirty(true);
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+  const onSubmit = async (data) => {
     try {
-      const workshopData = {
-        name: workshop.name,
-        description: workshop.description,
-        startDate: workshop.startDate,
-        endDate: workshop.endDate,
-        professor: workshop.professor
-      };
       const token = localStorage.getItem('user._id');
       const headers = { Authorization: `Bearer ${token}` };
-
-      await axios.post(process.env.NEXT_PUBLIC_URL_USER + 'workshop/create', workshopData, { headers });
+      await axios.post(process.env.NEXT_PUBLIC_URL_USER + 'workshop/create', data, { headers });
       toast.success('Workshop created successfully!');
       router.push('/workshops');
     } catch (error) {
-      if (error.response && error.response.data && error.response.data.error) {
-        const errorMessage = error.response.data.error.message;
-        const errorCode = error.response.data.error.code;
-        console.error(`Error code: ${errorCode}, Message: ${errorMessage}`);
-      } else {
-        console.error('An error occurred:', error);
-      }
+      toast.error('Error while creating workshop. Please try again.');
     }
-  };
-
-  const isFormValid = () => {
-    return Object.values(workshop).every((value) => value.trim() !== '') && isDirty;
   };
 
   return (
     <section className='container'>
-      <h1 className='main-title'>Create Workshop</h1>
-      <form className='form-container' onSubmit={handleSubmit}>
-        <input type='text' name='name' placeholder='Name' className='input' value={workshop.name} autoComplete='off' onChange={handleInputChange} />
-        <input type='text' name='description' placeholder='Description' className='input' value={workshop.description} autoComplete='off' onChange={handleInputChange} />
-        <input type='date' name='startDate' placeholder='Start Date' className='input' value={workshop.startDate} autoComplete='off' onChange={handleInputChange} />
-        <input type='date' name='endDate' placeholder='End Date' className='input' value={workshop.endDate} autoComplete='off' onChange={handleInputChange} />
-        <select name='professor' className='input' value={workshop.professor} autoComplete='off' onChange={handleInputChange}>
+      <h1 className='main-title'>Create New Workshop</h1>
+      <form className='form-container' onSubmit={handleSubmit(onSubmit)}>
+        <input type='text' placeholder='Name' autoComplete='off' className='input' {...register('name')} />
+        <p className='error-message'>{errors.name?.message}</p>
+        <textarea placeholder='Description' autoComplete='off' className='input min-h-32' {...register('description')} />
+        <p className='error-message'>{errors.description?.message}</p>
+        <input type='date' placeholder='Start Date' autoComplete='off' className='input' {...register('startDate')} />
+        <p className='error-message'>{errors.startDate?.message}</p>
+        <input type='date' placeholder='End Date' autoComplete='off' className='input' {...register('endDate')} />
+        <p className='error-message'>{errors.endDate?.message}</p>
+        <select name='professor' className='input' {...register('professor')}>
           <option value=''>Select Professor</option>
           {professors.map((professor) => (
             <option key={professor._id} value={professor._id}>
@@ -82,7 +82,10 @@ export default function WorkshopCreate() {
             </option>
           ))}
         </select>
-        <button type='submit' className={`button ${!isFormValid() ? 'disabled-button' : 'enabled-button'}`} disabled={!isFormValid()}>Create</button>
+        <p className='error-message'>{errors.professor?.message}</p>
+        <button type='submit' className={`button ${!isValid ? 'disabled-button' : 'enabled-button'}`} disabled={!isValid}>
+          Create
+        </button>
       </form>
     </section>
   );
